db6fd561643e9d9b1469f6c70ddee3a10ac5b497
Safouene Chemkhi <safouene.cs@gmail.com>
2019-12-01 04:36:51 +0100

    Adding bootstrap & Creating Login, Signup and Home components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path : '', component : LoginComponent },
+      { path : 'home', component : HomeComponent},
+      { path : 'login', component:LoginComponent},
+      { path : 'signup', component:SignupComponent}
+    ])
+
   ],
   providers: [],
   bootstrap: [AppComponent]
