415502df574852a4593d26bf758367ad3d0e46ac
Safouene Chemkhi <safouene.cs@gmail.com>
2019-12-01 16:13:30 +0100

    Fixing navbar & implementing logout func.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app';
+  authenticated : boolean = false;
+  constructor(public auth: AuthService, public router : Router){
+
+  }
+  ngOnInit(){
+    if (localStorage.getItem('token')){
+      this.authenticated = true;
+    }
+  }
+  logout(){
+    this.auth.logout();
+    this.router.navigateByUrl('/login');
+
+  }
 }
